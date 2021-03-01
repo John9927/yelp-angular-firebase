086da28c230dffed0744b7c8ce8e63454f25bd31
Giovanni Izzi <giovanni9972@libero.it>
2021-03-01 18:08:39 +0100

    add filter prezzo + fix styling

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,15 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
-import { Subject, Observable, combineLatest } from 'rxjs';
+import { Subject, combineLatest, BehaviorSubject, Observable } from 'rxjs';
+import firebase from 'firebase/app';
+import { switchMap } from 'rxjs/operators';
+
+export interface Item {
+  prezzo1: string;
+  prezzo2: string;
+  prezzo3: string;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -24,6 +32,15 @@ export class DashboardComponent implements OnInit {
 
   trova;
   vicino;
+  city: string | any;
+
+
+
+  items$: Observable<Item[]>
+  prezzo1$: BehaviorSubject<string|null>;
+  prezzo2$: BehaviorSubject<string|null>;
+  prezzo3$: BehaviorSubject<string|null>;
+
 
   startobs = this.startAt.asObservable();
   endobs = this.endAt.asObservable();
@@ -33,24 +50,59 @@ export class DashboardComponent implements OnInit {
     vicino: ['', [Validators.required]],
   });
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router, private afs: AngularFirestore) { }
+  constructor(private fb: FormBuilder, private router: Router, private afs: AngularFirestore, private authService: AuthService) {
+    this.prezzo1$ = new BehaviorSubject(null);
+    this.prezzo2$ = new BehaviorSubject(null);
+    this.prezzo3$ = new BehaviorSubject(null);
+    this.items$ = combineLatest([
+      this.prezzo1$,
+      this.prezzo2$,
+      this.prezzo3$,
+    ]).pipe(
+      switchMap(([prezzo1, prezzo2, prezzo3]) =>
+        afs.collection('vicino', ref => {
+          let query : firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
+          if (prezzo1) { query = query.where('prezzo', '==', prezzo1) };
+          if (prezzo2) { query = query.where('prezzo', '==', prezzo2) };
+          if (prezzo3) { query = query.where('prezzo', '==', prezzo3) };
+          return query;
+        }).valueChanges()
+      )
+    );
+  }
 
+  filterPrezzo1(prezzo1: string|null) {
+    this.prezzo1$.next(prezzo1);
+  }
+  filterPrezzo2(prezzo2: string|null) {
+    this.prezzo2$.next(prezzo2);
+  }
+  filterPrezzo3(prezzo3: string|null) {
+    this.prezzo3$.next(prezzo3);
+  }
 
   ngOnInit() {
 
-    const combinedTrova = combineLatest(this.startAt, this.endobs)
-    combinedTrova.subscribe((value) => {
+
+  combineLatest([this.startAt, this.endAt])
+    .subscribe((value) => {
       this.firequery(value[0], value[1]).subscribe((trova) => {
         this.trova = trova;
       })
     })
 
-    const combinedVicino = combineLatest(this.startAt, this.endobs)
-    combinedVicino.subscribe((value) => {
+    combineLatest([this.startAt, this.endAt])
+    .subscribe((value) => {
       this.firequery2(value[0], value[1]).subscribe((vicino) => {
         this.vicino = vicino;
       })
     })
+
+    this.authService.getCity()
+      .subscribe(res => {
+        this.city = res.docs.map(doc => doc.data())
+      }
+    )
 
   }
 
@@ -67,16 +119,26 @@ export class DashboardComponent implements OnInit {
   }
 
   firequery(start, end) {
-    return this.afs.collection('trova', ref => ref.limit(4).orderBy('trova').startAt(start).endAt(end)).valueChanges()
+    return this.afs.collection('trova', ref =>
+      ref.limit(4)
+      .orderBy('trova')
+      .startAt(start)
+      .endAt(end))
+      .valueChanges()
   }
 
   firequery2(start, end) {
-    return this.afs.collection('vicino', ref => ref.limit(4).orderBy('vicino').startAt(start).endAt(end)).valueChanges()
+    return this.afs.collection('vicino', ref =>
+      ref.limit(4)
+      .orderBy('vicino')
+      .startAt(start)
+      .endAt(end))
+      .valueChanges()
   }
 
   onSubmit() {
     console.log(this.profileForm.value);
-    this.router.navigateByUrl('home-page');
+    // this.router.navigateByUrl('home-page');
   }
 
   selectValueVicino(value) {
@@ -96,4 +158,5 @@ export class DashboardComponent implements OnInit {
   toggleDropDown2() {
     this.showDropDown2 = !this.showDropDown2;
   }
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import firebase from 'firebase';
-import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   constructor(private firestore: AngularFirestore) { }
+  getCity() {
+   return this.firestore
+    // .collection('vicino', ref =>ref)
+    // .get()
+    .collection('vicino', ref => ref.where('prezzo', '==', '€'))
+    .get()
+  }
+
+
 
 
 
